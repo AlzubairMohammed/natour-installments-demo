61d41765b803dba3c60afab6890a48a4adf6f057
AlzubairMohammed <dev.alzubair@gmail.com>
2024-02-19 20:07:21 +0300

    Edit installment user register

--- validations/installmentUserRegister.js
+++ validations/installmentUserRegister.js
@@ -4,20 +4,24 @@ exports.installmentUserRegisterValidator = () => {
   return [
     body("rent_id").isInt("Rent id must be integer ."),
     body("name")
-      .notEmpty("Name can't be null .")
-      .isString("Name must be string format ."),
-    body("email").isEmail(
-      "Email must be in email format like name@provider.com ."
-    ),
-    body("phone").isMobilePhone("Phone must in right format ."),
-    body("have_family").isBoolean("This field must be boolean ."),
-    body("salary").isDecimal("Field must be decimal ."),
+      .notEmpty()
+      .withMessage("Name can't be null .")
+      .isString()
+      .withMessage("Name must be string format ."),
+    body("email")
+      .isEmail()
+      .withMessage("Email must be in email format like name@provider.com ."),
+    body("phone").isMobilePhone().withMessage("Phone must in right format ."),
+    body("have_family").isBoolean().withMessage("This field must be boolean ."),
+    body("salary").isDecimal().withMessage("Field must be decimal ."),
     body("nationality").isString("Field must be string ."),
-    body("gender").isInt("Field must be integer ."),
+    body("gender").isInt().withMessage("Field must be integer ."),
     body("more_info"),
-    body("user_id").isEmpty("Field can't be null"),
+    body("user_id").isEmpty().withMessage("Field can't be null"),
     body("expect_rent_date")
-      .isDate("Field must be in date format")
-      .notEmpty("Field can't be null"),
+      .isDate()
+      .withMessage("Field must be in date format")
+      .notEmpty()
+      .withMessage("Field can't be null"),
   ];
 };
