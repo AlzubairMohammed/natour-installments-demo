aee6513ce312ff3ca3bc150640432c1de4e8e468
AlzubairMohammed <dev.alzubair@gmail.com>
2024-03-01 14:58:39 +0300

    Edit database query

--- backend/database/queries.sql
+++ backend/database/queries.sql
@@ -12,8 +12,8 @@ CREATE TABLE installment_user_register (
     user_id BIGINT UNSIGNED,
     expect_rent_date DATE,
     is_accepted BOOLEAN,
-    FOREIGN KEY (rent_id) REFERENCES rents(id),
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    FOREIGN KEY (rent_id) REFERENCES rents(id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
 CREATE TABLE out_appartments (
@@ -25,9 +25,9 @@ CREATE TABLE out_appartments (
     city_id BIGINT UNSIGNED,
     area_id BIGINT UNSIGNED,
     is_rented BOOLEAN,
-    FOREIGN KEY (register_id) REFERENCES installment_user_register(id),
-    FOREIGN KEY (city_id) REFERENCES cities(id),
-    FOREIGN KEY (area_id) REFERENCES areas(id)
+    FOREIGN KEY (register_id) REFERENCES installment_user_register(id) ON DELETE CASCADE,
+    FOREIGN KEY (city_id) REFERENCES cities(id) ON DELETE CASCADE,
+    FOREIGN KEY (area_id) REFERENCES areas(id) ON DELETE CASCADE
 );
 
 CREATE TABLE appartments (
@@ -44,8 +44,8 @@ CREATE TABLE appartments (
     lat VARCHAR(255),
     lng VARCHAR(255),
     status INT,
-    FOREIGN KEY (city_id) REFERENCES cities(id),
-    FOREIGN KEY (area_id) REFERENCES areas(id)
+    FOREIGN KEY (city_id) REFERENCES cities(id) ON DELETE CASCADE,
+    FOREIGN KEY (area_id) REFERENCES areas(id) ON DELETE CASCADE
 );
 
 CREATE TABLE installments (
@@ -55,8 +55,8 @@ CREATE TABLE installments (
     installment_start_date DATE,
     installment_end_date DATE,
     is_guarantee BOOLEAN,
-    FOREIGN KEY (appartment_id) REFERENCES appartments(id),
-    FOREIGN KEY (installment_user_register_id) REFERENCES installment_user_register(id)
+    FOREIGN KEY (appartment_id) REFERENCES appartments(id) ON DELETE CASCADE,
+    FOREIGN KEY (installment_user_register_id) REFERENCES installment_user_register(id) ON DELETE CASCADE
 );
 
 CREATE TABLE installment_months(
@@ -65,13 +65,13 @@ CREATE TABLE installment_months(
     installment_id BIGINT UNSIGNED,
     paid_date DATE,
     payment_method INT,
-    FOREIGN KEY (installment_id) REFERENCES installments(id)
+    FOREIGN KEY (installment_id) REFERENCES installments(id) ON DELETE CASCADE
 );
 
 CREATE TABLE appartment_images(
     id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     appartment_id BIGINT UNSIGNED,
     image VARCHAR(255),
-    FOREIGN KEY (appartment_id) REFERENCES appartments(id)
+    FOREIGN KEY (appartment_id) REFERENCES appartments(id) ON DELETE CASCADE
 );
 
