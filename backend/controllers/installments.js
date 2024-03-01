83dfc4903db215ffbee061b2c29fe6216a3a1cb8
AlzubairMohammed <dev.alzubair@gmail.com>
2024-03-01 03:05:25 +0300

    Edit get id api

--- backend/controllers/installments.js
+++ backend/controllers/installments.js
@@ -142,7 +142,7 @@ exports.deleteInstallment = asyncWrapper(async (req, res, next) => {
 });
 
 exports.getUserInstallment = asyncWrapper(async (req, res, next) => {
-  const user = await axios.get("http://localhost/api/get-id", {
+  const user = await axios.get("http://127.0.0.1:8000/api/get-id", {
     headers: req.headers,
   });
 
