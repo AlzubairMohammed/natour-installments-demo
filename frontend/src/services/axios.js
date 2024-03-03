ee4eddff0e857cffa4a117ad711534f0fda14cf2
AlzubairMohammed <dev.alzubair@gmail.com>
2024-03-03 21:39:15 +0300

    Server changes

--- frontend/src/services/axios.js
+++ frontend/src/services/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const instance = axios.create({
-  // baseURL: "http://inst.natour.ae/api/v1",
-  baseURL: "http://localhost:7070/api/v1",
+  baseURL: "http://inst.natour.ae/api/v1",
+  // baseURL: "http://localhost:7070/api/v1",
   timeout: 1000,
   //   headers: { "X-Custom-Header": "foobar" },
 });
